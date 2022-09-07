7b9b5358d9826a57c3750cbebc967ae5a2349cfb
bob.guo <bob.guo@aitos.io>
2022-09-07 15:48:08 +0800

    first commit

---
+++ app/src/main/java/com/bob/walletdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.bob.walletdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createWallet();
+    }
+
+    private void createWallet() {
+
+    }
+}
